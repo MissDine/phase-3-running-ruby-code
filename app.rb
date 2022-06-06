e9e5243a46e983896dabf65f019d600114c91f59
MissDine <enidnyatichi1212@gmail.com>
2022-06-06 10:51:38 +0300

    Feat:Edit

--- app.rb
+++ app.rb
@@ -7,3 +7,10 @@ puts "Hello Enid"
 print "Hello world!"
 print "Hello Dine"
 print "Hello Enid"
+puts " "
+# Inspecting data with p and pp
+
+# When using puts on something that isn't already a string, Ruby will coerce the data to a string by calling the .to_s method
+# In the case of arrays, this will produce the following output, with each element of the array printed to a new line in the console:
+# eg
+puts [1, 2, 3]
